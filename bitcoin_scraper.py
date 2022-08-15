9df2d2f99f8964cb332bfbab9f18d79fb8134e26
Luke Xue <luqi9900@gmail.com>
2022-08-14 18:44:29 -0600

    turned function

--- bitcoin_scraper.py
+++ bitcoin_scraper.py
@@ -3,20 +3,24 @@ import OpenBlender
 import json
 import config
 
-token = config.data_token
+def bitcoin_scrape(start="2022-05-22", end="2022-08-12"):
+    token = config.data_token
+    action = 'API_getObservationsFromDataset'
 
-action = 'API_getObservationsFromDataset'
+    parameters = {
+        'token': token,
+        'id_dataset': '5d4c3b789516290b02fe3e24',
+        'date_filter': {'start_date': start,'end_date': end},
+    }
 
-parameters = {
-    'token': token,
-    'id_dataset': '5d4c3b789516290b02fe3e24',
-    'date_filter': {'start_date': '2022-05-22','end_date': '2022-08-12'},
-}
+    response = OpenBlender.call(action, parameters)
 
-response = OpenBlender.call(action, parameters)
+    df = pd.read_json(json.dumps(response['sample']), convert_dates=False, convert_axes=False)
+    df['date'] = [OpenBlender.unixToDate(ts, timezone = 'GMT') for ts in df.timestamp]
 
-df = pd.read_json(json.dumps(response['sample']), convert_dates=False, convert_axes=False)
-df['date'] = [OpenBlender.unixToDate(ts, timezone = 'GMT') for ts in df.timestamp]
+    df['change'] = df['close'] - df['open']
+    df['binary_change'] = [1 if change > 0 else 0 for change in df['change']]
 
-df['change'] = df['close'] - df['open']
-df['binary_change'] = [1 if change > 0 else 0 for change in df['change']]
+    return df
+
+print(bitcoin_scrape(start="2022-05-22", end="2022-08-12"))
